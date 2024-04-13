f1187a5711d6aaade1fbf217089d40d053d65a6e
Newg <g_robert@insa-toulouse.fr>
2024-04-13 18:47:39 -0400

    Endpoints User

--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,15 +1,6 @@
-use std::env;
 use actix_web::{App, get, HttpServer, Responder};
-use diesel::{connection, PgConnection};
+use argon2::password_hash::PasswordHasher;
 use serde::{Deserialize, Serialize};
-use lazy_static::lazy_static;
-use crate::controller::database_manager::establish_connection;
-use std::sync::Mutex;
-use diesel::r2d2::ConnectionManager;
-use r2d2_postgres::{postgres, PostgresConnectionManager};
-use r2d2::PooledConnection;
-use r2d2_postgres::postgres::Config;
-
 
 mod view{
     pub mod client;
@@ -49,7 +40,7 @@ pub async fn index() -> impl Responder {
 
 /*fn main() {
     // Hash a password
-
+    /*
     let salt = generate_salt();
     let password_hash = hash_password("aled", &salt).unwrap();
 
@@ -72,45 +63,26 @@ pub async fn index() -> impl Responder {
         }
     };
     println!("{}", jwt);
+     */
+}
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
 
-} */
-
-
- /*lazy_static!{
-       pub static ref GLOBAL_CONNECTION: Mutex<PgConnection> = Mutex::new(establish_connection());
-     }*/
-type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-
-    dotenv::dotenv().ok();
-
-    /*let uri = match env::var("API_Address") {
-        Ok(uri) => uri,
-        Err(err) => {println!("Failed to get address: {}", err); return Err(std::io::Error::new(std::io::ErrorKind::Other, "Failed to get APIURI"))}
-    };*/
-
-    println!("Launched server ...");
     HttpServer::new(|| {
-
-        let url =  env::var("DATABASE_URL").unwrap() ;
-
-        let manager = ConnectionManager::<PgConnection>::new(url);
-        let pool = r2d2::Pool::builder()
-            .build(manager)
-            .expect("database URL should be valid path to Postgres DB file");
         App::new()
-            .app_data(actix_web::web::Data::new(pool))
             .service(index)
-            .service(view::client::login)
-            .service(view::client::register)
-            .service(view::client::get_users)
-            .service(view::client::get_user_by_username_email)
-            .service(view::client::add_dgs)
-            .service(view::client::dgs_login)
+            .service(view::user::add_user)
+            .service(view::user::get_users)
+            .service(view::user::get_user_by_username_email)
+            .service(view::user::login)
+            .service(view::user::register)
+            .service(view::user::update_user)
+            .service(view::user::delete_user)
+            .service(view::user::add_dgs)
+            .service(view::user::get_user_by_id)
+            .service(view::user::dgs_login)
     })
-        .bind("0.0.0.0:8080")?
+        .bind("127.0.0.1:8080")?
         .run()
         .await
 }
