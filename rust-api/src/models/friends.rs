da83e9615f2899eff643f313a123a2dc50cda495
lsacienne <alexandre.viala@outlook.com>
2024-04-16 01:26:26 -0400

    🗃️ Add friends model

---
+++ rust-api/src/models/friends.rs
@@ -0,0 +1,10 @@
+use diesel::{associations::Identifiable, deserialize::Queryable, Selectable, Insertable};
+use serde::{Deserialize, Serialize};
+
+#[derive(Queryable, Selectable, Insertable, Identifiable, Serialize, Deserialize)]
+#[diesel(table_name = crate::schema::friend)]
+#[diesel(primary_key(client1_id, client2_id))]
+pub struct Friend {
+    pub client1_id: uuid::Uuid,
+    pub client2_id: uuid::Uuid
+}
