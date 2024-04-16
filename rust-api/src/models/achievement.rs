aa8790a62f4f138c32843cb07a6438363e4d4f8e
lsacienne <alexandre.viala@outlook.com>
2024-04-16 01:26:48 -0400

    🗃️ Add achievements model

---
+++ rust-api/src/models/achievement.rs
@@ -0,0 +1,23 @@
+use diesel::{associations::{Identifiable, Associations}, deserialize::Queryable, Selectable};
+use serde::{Deserialize, Serialize};
+use crate::models::client::Client;
+
+#[derive(Queryable, Selectable, Serialize, Identifiable, Deserialize)]
+#[diesel(table_name = crate::schema::achievement)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Achievement {
+    pub id: uuid::Uuid,
+    pub name: String,
+    pub description: String,
+    pub image: Option<String>
+}
+
+#[derive(Queryable, Selectable, Associations, Identifiable, Serialize, Deserialize)]
+#[diesel(table_name = crate::schema::client_achievement)]
+#[diesel(belongs_to(Client))]
+#[diesel(belongs_to(Achievement))]
+#[diesel(primary_key(client_id, achievement_id))]
+pub struct ClientAchievement {
+    pub client_id: uuid::Uuid,
+    pub achievement_id: uuid::Uuid
+}
