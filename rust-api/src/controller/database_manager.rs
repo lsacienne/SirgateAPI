361663110dae7ada553ab082dda88cf46717eee0
lsacienne <alexandre.viala@outlook.com>
2024-04-17 00:02:30 -0400

    🗃️ Add function to connect to redis

--- rust-api/src/controller/database_manager.rs
+++ rust-api/src/controller/database_manager.rs
@@ -1,6 +1,7 @@
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenvy::dotenv;
+use redis::RedisResult;
 use std::env;
 
 pub fn establish_connection() -> PgConnection {
@@ -10,4 +11,14 @@ pub fn establish_connection() -> PgConnection {
         .expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn establish_redis_connection() -> RedisResult<redis::Connection> {
+    dotenv().ok();
+
+    let redis_url = env::var("REDIS_URL")
+        .expect("REDIS_URL must be set");
+    let client = redis::Client::open(redis_url)
+        .unwrap();
+    client.get_connection()
 }
