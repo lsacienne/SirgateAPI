1172ee97b9529d87ed67c7b7dd75f3cf699f6bde
Newg <g_robert@insa-toulouse.fr>
2024-04-18 18:21:48 -0400

    Implémentation login

--- rust-api/src/view/client.rs
+++ rust-api/src/view/client.rs
@@ -1,17 +1,13 @@
 use std::hash;
-use actix_web::{error, HttpResponse, Responder, web};
+use actix_web::{HttpResponse, Responder, web};
 use argon2::{Argon2, PasswordHash, PasswordHasher};
 use argon2::password_hash::rand_core::OsRng;
 use argon2::password_hash::SaltString;
 use jsonwebtoken::{EncodingKey, Header};
-use r2d2_postgres::{postgres, PostgresConnectionManager};
-use r2d2_postgres::postgres::fallible_iterator::FallibleIterator;
-//use crate::GLOBAL_CONNECTION;
+use crate::GLOBAL_CONNECTION;
 use crate::Claims;
 use crate::controller::client::{add_user, get_user_by_email, get_user_by_username};
 use crate::models::client::{DGS, Client, ClientAuth, InsertableClient};
-use crate::DbPool;
-
 
 /*#[actix_web::post("/users")]
 pub async fn add_user(user: web::Json<models::user>) -> impl Responder {
@@ -42,12 +38,11 @@ fn hash_password<'a>(password: &'a str, salt: &'a SaltString) -> Result<Password
 
     Ok(password_hash)
 }
-
 fn generate_salt() -> SaltString {
     SaltString::generate(&mut OsRng)
 }
-
 fn create_jwt(claims: Claims) -> Result<String, jsonwebtoken::errors::Error> {
+
     let mut header = Header::new(jsonwebtoken::Algorithm::HS256);
     header.typ = Some("JWT".to_string());
 
@@ -56,81 +51,67 @@ fn create_jwt(claims: Claims) -> Result<String, jsonwebtoken::errors::Error> {
 
     Ok(jwt?)
 }
-
 #[actix_web::post("/login")]
-pub async fn login(pool: web::Data<DbPool>, user: web::Json<ClientAuth>) -> actix_web::Result<impl Responder>  {
+pub async fn login(user: web::Json<ClientAuth>) -> impl Responder {
     // Deserialize JSON to User struct
     let _user: ClientAuth = user.into_inner();
 
-    if _user.password.is_empty() {
-        return Ok(HttpResponse::BadRequest().body("Password is empty"));
+    // TODO get user from db
+    let mut conn =&mut GLOBAL_CONNECTION.lock().unwrap();
+
+    let mut user: Client;
+
+    if(_user.password.is_empty()){
+        return HttpResponse::BadRequest().body("Password is empty")
+    }
+    if(_user.email.is_empty() && _user.username.is_empty()){
+        return HttpResponse::BadRequest().body("Email or username is empty")
+    }else if(!_user.username.is_empty()){
+        user = get_user_by_username(conn, &*_user.username);
+    } else {
+        user = get_user_by_email(conn, &*_user.email);
     }
 
-    if _user.email.is_empty() && _user.username.is_empty() {
-         return Ok(HttpResponse::BadRequest().body("Email or username is empty"));
-    }
-    let client = web::block(move || {
-        // Obtaining a connection from the pool is also a potentially blocking operation.
-        // So, it should be called within the `web::block` closure, as well.
-        let mut conn = pool.get().expect("couldn't get db connection from pool");
 
-        let mut user: Client;
-
-
-        if (!_user.username.is_empty()) {
-            user = get_user_by_username(&mut *conn, &*_user.username);
-        } else {
-            user = get_user_by_email(&mut *conn, &*_user.email);
-        }
-        user
-    }).await?;
-
-    let salt = &mut SaltString::from_b64(&*client.salt).unwrap();
-    let hashed_password = hash_password(&*_user.password, salt).unwrap();
+    let aled =& mut SaltString::from_b64(&*user.salt).unwrap();
+    let hashed_password = hash_password(&*_user.password,aled).unwrap();
 
     let argon2 = Argon2::default();
 
-    if hashed_password.hash.unwrap().to_string() == client.password {
+    if hashed_password.hash.unwrap().to_string() == user.password{
         let claims = Claims {
-            iss: client.id.clone().to_string(),
-            sub: client.email.clone(),
+            iss: user.id.clone().to_string(),
+            sub: user.email.clone(),
             iat: 0,
             exp: 0,
         };
-         Ok(HttpResponse::Ok().body(create_jwt(claims).unwrap()))
+        return HttpResponse::Ok().body(create_jwt(claims).unwrap())
     } else {
-         Ok(HttpResponse::BadRequest().body("Invalid password"))
+        return HttpResponse::BadRequest().body("Invalid password")
     }
+
+
+
 }
 
 #[actix_web::post("/register")]
-pub async fn register(pool: web::Data<DbPool>, user: web::Json<ClientAuth>) -> actix_web::Result<impl Responder> {
+pub async fn register(user: web::Json<ClientAuth>) -> impl Responder {
+
     let user: ClientAuth = user.into_inner();
 
+    let salt = generate_salt();
+    let hash= hash_password(&*user.password, &salt).unwrap();
 
-    let client = web::block(move || {
-        // Obtaining a connection from the pool is also a potentially blocking operation.
-        // So, it should be called within the `web::block` closure, as well.
-
-
-        let salt = generate_salt();
-        let hash = hash_password(&*user.password, &salt).unwrap();
-
-        let mut conn = pool.get().expect("couldn't get db connection from pool");
-
-        add_user(&mut *conn, &*user.username, &*user.email, &*hash.hash.unwrap().to_string(), salt.as_ref())
-    })
-    .await?;
-
-
+    let mut conn =&mut GLOBAL_CONNECTION.lock().unwrap();
+    let client = add_user(conn, &*user.username,&* user.email,&*hash.to_string(), salt.as_ref());
 
     let claims = Claims {
         iss: client.id.clone().to_string(),
-        sub: client.email.clone().to_string(),
+        sub: user.email.clone().to_string(),
         iat: 0,
         exp: 0,
     };
-    Ok(HttpResponse::Ok().body(create_jwt(claims).unwrap()))
+    create_jwt(claims).unwrap()
 }
 
 
@@ -150,7 +131,7 @@ pub async fn get_user_by_username_email(user: web::Json<ClientAuth>) -> impl Res
 }
 
 #[actix_web::post("/dgs/add")]
-pub async fn add_dgs(server: web::Json<DGS>) -> impl Responder {
+pub async fn add_dgs( server: web::Json<DGS>) -> impl Responder {
     // Here you can add the user to the database.
     // For now, let's just return the user data as JSON.
     HttpResponse::Ok().body(format!("Add DGS: {}", server.label))
@@ -166,6 +147,5 @@ pub async fn dgs_login(server: web::Json<DGS>) -> impl Responder {
         iat: 0,
         exp: 0,
     };
-    println!("FFFFFF!");
     HttpResponse::Ok().body(create_jwt(claims).unwrap())
 }
