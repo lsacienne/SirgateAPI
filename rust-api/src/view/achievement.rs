a03e8734834da2d0772e3b5c66b9fd66b813d1cc
lsacienne <alexandre.viala@outlook.com>
2024-04-22 17:14:16 -0400

    Add get all achievement endpoint

--- rust-api/src/view/achievement.rs
+++ rust-api/src/view/achievement.rs
@@ -1,7 +1,10 @@
 use actix_web::{HttpResponse, Responder, web};
+use uuid::Uuid;
+
+use crate::DbPool;
 
 #[actix_web::post("/achievement/add")]
-pub async fn add_achievement(achievement: web::Json<String>) -> impl Responder {
+pub async fn add_achievement(pool: web::Data<DbPool>, achievement: web::Json<String>) -> impl Responder {
     // Deserialize JSON to Achievement struct
     let Achievement = achievement.into_inner();
 
@@ -12,8 +15,18 @@ pub async fn add_achievement(achievement: web::Json<String>) -> impl Responder {
 }
 
 #[actix_web::get("/achievement/getall")]
-pub async fn get_all_achievements(client: web::Json<String>) -> impl Responder {
+pub async fn get_all_achievements(pool: web::Data<DbPool>, client: web::Json<String>) -> actix_web::Result<impl Responder> {
     // Here you can add the user to the database.
-    // For now, let's just return the user data as JSON.
-    " Get All Achievements you scumbag !"
+    let client_id_string = client.into_inner();
+    let id = Uuid::parse_str(&client_id_string).unwrap();
+
+    let achievements = web::block(move || {
+        // Obtaining a connection from the pool is also a potentially blocking operation.
+        // So, it should be called within the `web::block` closure, as well.
+        let mut conn = pool.get().expect("couldn't get db connection from pool");
+
+        crate::controller::achievement::get_all_achievements(&mut conn, id)
+    }).await?;
+
+    Ok(HttpResponse::Ok().json(achievements))
 }
